8bc1e6ebd78a3ecb106c96b2b8cc544878d939a4
TheLocagus <locagus@o2.pl>
2022-10-09 01:21:00 +0200

    Created component Button

---
+++ src/components/Button.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  export let href = null;
+  export let url = null;
+  export let type = null;
+  export let onClick = null;
+  export let text = null;
+  export let className = null;
+</script>
+
+{#if !href}
+    <button type="{type}" class="{className}" on:click={onClick}>{text}</button>
+{:else}
+    <a class="{className}" href="{url}">{text}</a>
+{/if}
+
+<style>
+    .todo-button {
+        width: 70px;
+        margin: 1px 0;
+        padding: 2px 5px;
+        font-size: 12px;
+        background-color: #de7249;
+        border: 1px solid black;
+        border-radius: 5px;
+        color: #444;
+        cursor: pointer;
+    }
+
+    .todo-button:hover {
+        background-color: orangered;
+        border: 1px solid black;
+    }
+
+    .add-todo {
+        padding: 15px 70px;
+        background-color: orangered;
+        border: 1px solid orangered;
+        color: #030303;
+        font-weight: bold;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    .add-todo:hover {
+        background-color: #de7249;
+        border: 1px solid orangered;
+    }
+</style>
